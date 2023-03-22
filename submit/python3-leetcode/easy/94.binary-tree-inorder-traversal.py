bd4e8cf929986ca838f70393df1a4e96008018d0
liufeng02 <liufeng02@inspur.com>
2023-03-22 15:36:10 +0800

    二叉树遍历通用方法

--- submit/python3-leetcode/easy/94.binary-tree-inorder-traversal.py
+++ submit/python3-leetcode/easy/94.binary-tree-inorder-traversal.py
@@ -6,14 +6,25 @@
 
 # @lc code=start
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 from typing import Optional,List
 class Solution:
+    # def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    #     return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right) if root else []
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right) if root else []
+        r_,s_ = [],[root]
+        while s_:
+            n_ = s_.pop()
+            if isinstance(n_,TreeNode):
+                s_.extend([n_.right,n_.val,n_.left])
+            elif isinstance(n_,int):
+                r_.append(n_)
+            
+        return r_
+
 # @lc code=end
 
