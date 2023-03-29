2ac2fd97db4cec97f19808ba43f84777d50ceeec
liufeng02 <liufeng02@inspur.com>
2023-03-29 09:34:34 +0800

    单链表中间节点

--- huawei/2022.Q4/006.单向链表中间节点.100.py
+++ huawei/2022.Q4/006.单向链表中间节点.100.py
@@ -21,16 +21,49 @@
 输出:
 6
 '''
-from typing import Optional
-class LinkedListNode:
-    def __init__(self, val:int=0,next=None):
-        self.val = val
-        self.next = next
+# [构造链表方法]
+def middle_node1():
+    class Node:
+        def __init__(self, val:int=0,next=None):
+            self.val = val
+            self.next = next
 
-head,n_ = input().strip().split()
-n_ = int(n_)
-
-n_map = {}
-for i in range(n_):
-    n_addr,n_val,n_next = input().strip().split()
+    head,n_ = input().strip().split()
+    n_ = int(n_)
+    n_map = {} #addr:Node
+    # 1. 根据每行输入构造字典n_map = {addr:Node},此时Node的next属性存放下一个节点的地址
+    for i in range(n_):
+        n_addr,n_val,n_next = input().strip().split()
+        n_map[n_addr] = Node(n_val,n_next)
+    # 2. 遍历字典将每个Node的next设置为实际的下一个节点Node本身
+    for n in n_map:
+        n_map[n].next = n_map[n_map[n].next] if n_map[n].next != '-1' else None
+    # 3. 设置头,循环遍历n/2次,找到中间节点
+    head = n_map[head]
+    size_div2 = n_//2+1 if n_%2 else n_//2 # 如果奇数个节点取中间，偶数个取偏右边的那个值
+    for i in range(size_div2):
+        head = head.next
+    print(head.val)
+    # while node != None:
+    #     print(node.val)
+    #     node = node.next
+# [字典查找方法]
+def middle_node2():
+    head,n_ = input().strip().split()
+    n_ = int(n_)
+    n_map = {} #addr:node
+    # 1. 获取输入构造字典,value不是Node 而是dict = {'val': n_val, 'next': n_next}
+    for i in range(n_):
+        n_addr,n_val,n_next = input().strip().split()
+        n_map[n_addr] = {
+            'val':n_val,
+            'next':n_next
+        }
+    # 2. 遍历字典,循环n/2次, 找到中间节点
+    size_div2 = n_//2+1 if n_%2 else n_//2
+    now = n_map[head]
+    for i in range(size_div2):
+        now = n_map[now['next']]
+    print(now['val'])
     
+middle_node2()
